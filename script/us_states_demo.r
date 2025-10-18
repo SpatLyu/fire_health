aae61aa16756455f8fd7c13f15a4836fd3bd3496
SpatLyu <lyu.geosocial@gmail.com>
2025-10-18 12:34:10 +0800

    update

--- script/us_states_demo.r
+++ script/us_states_demo.r
@@ -1,4 +1,6 @@
 require("magrittr")
+require("geofacet")
+source('./script/utils.r')
 
 diseases = fs::dir_ls('./data/gbd/',type = "directory") |> 
   stringr::str_sub(12,-1)
@@ -11,15 +13,13 @@ dt = readr::read_csv('./data/us_fire_health.csv') |>
 us.states = unique(dt$state)
 match(us.states,geofacet::us_state_grid2$name)
 
-source('./script/utils.r')
-
 analysis_diseases = \(state_name,disease = "Cardiovascular diseases",lag = 0){
   df = dt |> 
     dplyr::filter(state == state_name) |>
     dplyr::select(-state)
   
   res <- tryCatch({
-    invisible(infocausality::surd(df, "Cardiovascular diseases",
+    invisible(infocausality::surd(df, disease,
                                   c("fire_area","co","o3","pm10","no2","pm25"),
                                   lag = lag, bin = 5, cores = 4))
   }, error = function(e) {
@@ -34,45 +34,66 @@ analysis_diseases = \(state_name,disease = "Cardiovascular diseases",lag = 0){
            dplyr::mutate(state = state_name))
 }
 
-causation_lag0 = purrr::map_dfr(us.states,analysis_diseases) |> 
-  dplyr::filter(type != "info leak") |> 
-  dplyr::mutate(label = dplyr::case_match(
-    label,
-    "U[X1]" ~ "Fire",
-    "U[X2]" ~ "CO",
-    "U[X3]" ~ "O3",
-    "U[X4]" ~ "PM10",
-    "U[X5]" ~ "NO2",
-    "U[X6]" ~ "PM2.5",
-  )) 
+plot_causation_us = \(disease = "Cardiovascular diseases",lag = 0){
+  causation_lag = purrr::map_dfr(us.states,
+                                 \(.x) analysis_diseases(.x,disease,lag)) |> 
+    dplyr::filter(type != "info leak") |> 
+    dplyr::mutate(label = dplyr::case_match(
+      label,
+      "U[X1]" ~ "Fire",
+      "U[X2]" ~ "CO",
+      "U[X3]" ~ "O3",
+      "U[X4]" ~ "PM10",
+      "U[X5]" ~ "NO2",
+      "U[X6]" ~ "PM2.5",
+    )) 
+  
+  ggplot2::ggplot(causation_lag, 
+                  ggplot2::aes(label, value, fill = label)) +
+    ggplot2::geom_col() +
+    ggplot2::coord_flip() +
+    geofacet::facet_geo(~ state,grid = "us_state_grid2") +
+    ggplot2::scale_x_discrete(name = paste0(disease," with Lag ",lag)) +
+    ggplot2::scale_y_continuous(
+      name = NULL,
+      breaks = function(x) {
+        rng = range(x, na.rm = TRUE)
+        pretty(rng, n = 3)
+      },
+      expand = ggplot2::expansion(mult = c(0, 0.05))
+    ) +
+    ggplot2::scale_fill_discrete(name = NULL) +  
+    ggplot2::guides(fill = ggplot2::guide_legend(
+      direction = "horizontal",
+      nrow = 1
+    )) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
+      legend.position = "inside", 
+      legend.justification = c("right", "bottom"))
+}
 
-causation_lag0 = causation_lag0 |> 
-  dplyr::bind_rows(tidyr::crossing(label = c("Fire","CO","O3","PM10","NO2","PM2.5"),
-                                   value = 1e-10,
-                                   type = "unique",
-                                   state = us.states[-match(unique(causation_lag0$state), us.states)]))
+for (i in seq_along(diseases)) {
+  purrr::map(0:3,\(.x) 
+             ggview::save_ggplot(plot_causation_us(diseases[i],.x) + ggview::canvas(10,8),
+                                 paste0("./figure/",diseases[i],"_causation_lag",.x,".pdf"),
+                                 device = cairo_pdf))
+}
 
-require("geofacet")
-cl0 = ggplot2::ggplot(causation_lag0, 
-                ggplot2::aes(label, value, fill = label)) +
-  ggplot2::geom_col() +
-  ggplot2::coord_flip() +
-  geofacet::facet_geo(~ state,grid = "us_state_grid2") +
-  ggplot2::scale_y_continuous(
-    name = NULL,
-    breaks = function(x) {
-      rng = range(x, na.rm = TRUE)
-      pretty(rng, n = 3)
-    },
-    expand = ggplot2::expansion(mult = c(0, 0.05))
-  ) +
-  ggplot2::scale_fill_discrete(name = "Variable") +
-  ggplot2::theme_bw()
 
-cl0 + ggview::canvas(10,8)
+fig1 = patchwork::wrap_plots(purrr::map(0:3,\(.x) 
+                                        plot_causation_us(diseases[1],.x)), 
+                             ncol = 2) +
+  #patchwork::plot_layout(guides = "collect") +
+  patchwork::plot_annotation(tag_levels = 'a')
 
-a = analysis_diseases("Texas",1)
-a = analysis_diseases("California",1)
+fig1 + ggview::canvas(20,16)
+ggview::save_ggplot(fig1 + ggview::canvas(20,16),
+                    paste0("./figure/",diseases[1],"_causation.pdf"),
+                    device = cairo_pdf)
+
+plot_causation_us(diseases[1],3) + ggview::canvas(10,8)
 
 cal_df = dt |> 
   dplyr::filter(state == "California") |> 
